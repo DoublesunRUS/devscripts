006a28c091bca233d8066ca3557a76e2b0e8f5ea
Капралов Александр <capralow@gmail.com>
2021-09-28 01:38:31 +0300

    АвтоФичи VA

---
+++ VA-Тесты/ЗапуститьТесты.ps1
@@ -0,0 +1,5 @@
+﻿chcp 65001
+
+executor_j11 -s АвтофичиVA.sbsl -m CreateFeatures "AFParams.json"
+executor_j11 -s АвтофичиVA.sbsl -m RunTests "AFParams.json"
+executor_j11 -s АвтофичиVA.sbsl -m OpenReport "AFParams.json"
---
+++ VA-Тесты/СоздатьФичи.ps1
@@ -0,0 +1,6 @@
+﻿chcp 65001
+
+executor_j11 -s АвтофичиVA.sbsl -m CreateFeatures "AFParams.json"
+
+Write-Host "Press any key to continue..."
+[void][System.Console]::ReadKey($true)
